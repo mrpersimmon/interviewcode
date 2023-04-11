fc3fb989ae5aea9fe9b81d270d22951fd6dd4c34
mrpersimmon <2218443280@qq.com>
2023-04-11 23:49:42 +0800

    Change filename: Template_NSum: twoSum 去重

--- src/nowcoder/hashmap_twopointers/Template_NSum.java
+++ src/nowcoder/hashmap_twopointers/Template_NSum.java
@@ -1,6 +1,8 @@
 package nowcoder.hashmap_twopointers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Template_NSum {
     // 目标：返回「一对儿」 nums 中能够凑出 target 的两个元素的值。
@@ -23,4 +25,56 @@ public class Template_NSum {
     }
 
     // nums 可能有多对儿元素之和都等于 target，返回所有和为 target 的元素对儿，不能重复。
+    public ArrayList<ArrayList<Integer>> twoSum(ArrayList<Integer> nums, int target) {
+        // 解决的重点在于：当 sum == target 时，要跳过所有重复的元素。并且所有的分支都应该跳过重复的元素。
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Collections.sort(nums); // 排序
+        int lo = 0, hi = nums.size() - 1; // 左右指针
+        while (lo < hi) {
+            int left = nums.get(lo), right = nums.get(hi);
+            int sum = left + right;
+            if (sum == target) {
+                ans.add(new ArrayList<>(Arrays.asList(left, right)));
+                while (lo < hi && nums.get(lo) == left) // 左指针去重
+                    lo++;
+                while (lo < hi && nums.get(hi) == right) // 右指针去重
+                    hi--;
+            } else if (sum < target) {
+                while (lo < hi && nums.get(lo) == left)
+                    lo++;
+            } else if (sum > target) {
+                while (lo < hi && nums.get(hi) == right)
+                    hi--;
+            }
+        }
+        return ans;
+    }
+
+//    public ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> nums, int target) {
+//        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+//        Collections.sort(nums);
+//        for (int i = 0; i < nums.size())
+//    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
