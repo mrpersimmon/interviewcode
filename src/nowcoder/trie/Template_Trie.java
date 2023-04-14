cdc43899360d31b54c229f9f442a62b65ea9c75c
mrpersimmon <2218443280@qq.com>
2023-04-14 22:54:12 +0800

    #tire#新增#：新增工具函数 getNode，新增 containsKey 和 get 方法。

--- src/nowcoder/trie/Template_Trie.java
+++ src/nowcoder/trie/Template_Trie.java
@@ -9,7 +9,39 @@ public class Template_Trie {
 
 }
 
+// TrieMap 类中一定需要记录 Trie 的根节点 root，以及 Trie 树中的所有节点数量用于实现 size() 方法
+
 class TrieMap<V> {
+    // ASCII 码个数
+    private static final int R = 256;
+    // 当前存在 Map 中的键值对个数
+    private int size = 0;
+
+    private static class TrieNode<V> {
+        V val = null;
+        TrieNode<V>[] children = new TrieNode[R];
+    }
+
+    // Trie 树的根节点
+    private TrieNode<V> root = null;
+
+    // 实现一个工具函数 getNode
+    // 从节点 node 开始搜索 key，如果存在返回对应节点，否则返回 null
+    private TrieNode<V> getNode(TrieNode<V> node, String key) {
+        TrieNode<V> p = node;
+        // 从节点 node 开始搜索 key
+        for (int i = 0; i < key.length(); i++) {
+            if (p == null)
+                // 无法向下搜索
+                return null;
+            // 向下搜索
+            char c = key.charAt(i);
+            p = p.children[c];
+        }
+        return p;
+    }
+
+
     /***** 增、改 ******/
 
     // 在 Map 中添加 key
@@ -27,12 +59,24 @@ class TrieMap<V> {
     // 搜索 key 对应的值，不存在则返回 null
     // get("the") -> 4
     // get("tha") -> null
-    public V get(String key);
+    public V get(String key) {
+        // 从 root 开始搜索 key
+        TrieNode<V> x = getNode(root, key);
+        if (x == null || x.val == null) {
+            // x 为空或 x 的 val 字段为空都说明 key 没有对应的值
+            return null;
+        }
+        return x.val;
+    }
 
     // 判断 key 是否存在在 Map 中
     // containsKey("tea") -> false
     // containsKey("team") -> true
-    public boolean containsKey(String key);
+    // 就算 getNode(key) 的返回值 x 非空，也只能说字符串 key 是一个前缀；
+    // 除非 x.val 同时非空，才能判断键 key 存在。
+    public boolean containsKey(String key) {
+        return get(key) != null;
+    }
 
     // 在 Map 的所有键中搜索 query 的最短前缀
     // shortestPrefixOf("themxyz") -> "the"
@@ -61,5 +105,7 @@ class TrieMap<V> {
     public boolean hasKeyWithPattern(String pattern);
 
     // 返回 Map 中键值对的数量
-    public int size();
+    public int size() {
+        return size;
+    };
 }
