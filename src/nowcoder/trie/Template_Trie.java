6d748d6207a38e9e06f445258a3b15f91349e249
mrpersimmon <2218443280@qq.com>
2023-04-16 23:45:32 +0800

    #tire#修改#：nexts -> children, HashMap<Integer, TrieNode<V>>  -> HashMap<Character, TrieNode<V>>

--- src/nowcoder/trie/Template_Trie.java
+++ src/nowcoder/trie/Template_Trie.java
@@ -8,13 +8,16 @@ public class Template_Trie {
     public static void main(String[] args) {
         TrieMap<Integer> map = new TrieMap<>();
         map.put("them", 1);
+//        map.put("them", 1);
         map.put("zip", 2);
         map.put("team",3);
         map.put("the", 4);
         map.put("app", 5);
         map.put("that", 6);
-//        System.out.println(map.shortestPrefixOf("themxyc"));
-//        System.out.println(map.longestPrefixOf("themxyz"));
+//        System.out.println(map.countWordsEqualTo("them"));
+//        System.out.println(map.countWordsStartingWith("th"));
+        System.out.println(map.shortestPrefixOf("themxyc"));
+        System.out.println(map.longestPrefixOf("themxyz"));
 //        List<String> th = map.keysWithPrefix("t");
 //        for (String s : th) {
 //            System.out.print(s + " ");
@@ -37,13 +40,13 @@ class TrieMap<V> {
         V val;
         int pass; // 表示有多少个单词共用这个节点
         int end; // 表示有多少个单词以这个节点结尾
-        public HashMap<Integer, TrieNode<V>> nexts; // <字符路径(字符 ASCII 码值)，下级节点>
+        HashMap<Character, TrieNode<V>> children; // <字符路径(字符 ASCII 码值)，下级节点>
 
         public TrieNode() {
             V val = null;
             pass = 0;
             end = 0;
-            nexts = new HashMap<>();
+            children = new HashMap<>();
         }
     }
     // 当前存在 Map 中的键值对个数
@@ -68,20 +71,17 @@ class TrieMap<V> {
         if (!containsKey(key)) {
             size++;
         }
-        // TODO: !containsKey(key)
         if (key == null) {
             return;
         }
         TrieNode<V> node = root; // 从根节点开始
         node.pass++;
-
-        int path = 0; // 字符路径
         for (int i = 0; i < key.length(); i++) { // 遍历字符串
-            path = key.charAt(i); // 确定字符路径
-            if (!node.nexts.containsKey(path)) { // 如果路径下面的节点是空的
-                node.nexts.put(path, new TrieNode<>()); // 新建节点
+            char path = key.charAt(i); // 确定字符路径
+            if (!node.children.containsKey(path)) { // 如果路径下面的节点是空的
+                node.children.put(path, new TrieNode<>()); // 新建节点
             }
-            node = node.nexts.get(path); // node 向下移动
+            node = node.children.get(path); // node 向下移动
             node.pass++; // 经过的节点 pass + 1
         }
         // key 的路径已插入完成，将值 val 存入节点
@@ -97,14 +97,13 @@ class TrieMap<V> {
         }
         TrieNode<V> node = root;
         node.pass--;
-        int path = 0;
         for (int i = 0; i < key.length(); i++) {
-            path = key.charAt(i);
-            if (--node.nexts.get(path).pass == 0) {
-                node.nexts.remove(path);
+            char path = key.charAt(i);
+            if (--node.children.get(path).pass == 0) {
+                node.children.remove(path);
                 return;
             }
-            node = node.nexts.get(path);
+            node = node.children.get(path);
         }
         node.end--;
     }
@@ -116,20 +115,21 @@ class TrieMap<V> {
     // 搜索 key，如果存在返回「对应节点」，否则返回 null
     private TrieNode<V> getNode(TrieNode<V> node, String key) {
         // 从根节点开始搜索 key
-        int path = 0;
         for (int i = 0; i < key.length(); i++) {
-            path = key.charAt(i); // 确定路径
-            if (!node.nexts.containsKey(path)){ // 路径下方节点不存在
+            char path = key.charAt(i); // 确定路径
+            if (!node.children.containsKey(path)){ // 路径下方节点不存在
                 // 无法向下搜索
                 return null;
             }
             // 向下搜索
-            node = node.nexts.get(path);
+            node = node.children.get(path);
         }
         return node;
     }
 
     // 搜索 key 对应的值(val)，不存在则返回 null
+    // get("the") -> 4
+    // get("tha") -> null
     public V get(String key) {
         TrieNode<V> x = getNode(root, key);
         // x 为空或 x 的 val 字段为空都说明 key 没有对应的值
@@ -140,23 +140,27 @@ class TrieMap<V> {
     }
 
     // 判断 key 是否存在于 Map 中
+    // containsKey("tea") -> false
+    // containsKey("team") -> true
     public boolean containsKey(String key) {
         return get(key) != null;
     }
 
     // 判断是否存在前缀为 prefix 的键
+    // hasKeyWithPrefix("tha") -> true
+    // hasKeyWithPrefix("apple") -> false
     public boolean hasKeyWithPrefix(String prefix) {
         // 只要能找到 prefix 对应的节点，就是存在前缀
         return getNode(root, prefix) != null;
     }
 
     // 在所有「键」中寻找 query 的最短前缀
+    // shortestPrefixOf("themxyz") -> "the"
     public String shortestPrefixOf(String query) {
 
         TrieNode<V> node = root;
-        int path = 0;
         for (int i = 0; i < query.length(); i++) {
-            path = query.charAt(i);
+
             if (node == null) {
                 // 无法向下搜索
                 return "";
@@ -165,7 +169,8 @@ class TrieMap<V> {
                 // 找到一个『键』是 query 的前缀
                 return query.substring(0, i);
             }
-            node = node.nexts.get(path);
+            char path = query.charAt(i);
+            node = node.children.get(path);
         }
         if (node != null && node.val != null) {
             // 如果 query 本身就是一个键
@@ -179,9 +184,8 @@ class TrieMap<V> {
         // 记录前缀的最大长度
         int max_len = 0;
         TrieNode<V> node = root;
-        int path = 0;
         for (int i = 0; i < query.length(); i++) {
-            path = query.charAt(i);
+
             if (node == null) {
                 // 无法向下搜索
                 break;
@@ -189,7 +193,9 @@ class TrieMap<V> {
             if (node.val != null) {
                 max_len = i; // 找到一个键是 query 的前缀，更新前缀的最大长度
             }
-            node = node.nexts.get(path); // 向下搜索
+            // 向下搜索
+            char path = query.charAt(i);
+            node = node.children.get(path);
         }
         if (node != null && node.val != null) {
             return query; // 如果 query 本身就是一个键，那么这就是最长前缀
@@ -200,6 +206,7 @@ class TrieMap<V> {
 
 
     // ------------- 搜索所有前缀为 prefix 的键 -------------
+    // keysWithPrefix("th") -> ["that", "the", "them"]
     public List<String> keysWithPrefix(String prefix) {
         // 先利用 getNode 在 Trie 树中找到 prefix 对应的节点 x
         // 使用多叉树的遍历算法，遍历以 x 为根的这棵 Trie 树，找到所有的键值对
@@ -224,10 +231,10 @@ class TrieMap<V> {
             res.add(path.toString());
         }
         // 回溯算法遍历框架
-        for (int c : node.nexts.keySet()) {
+        for (char c : node.children.keySet()) {
             // 做选择
-            path.append((char)c);
-            traverse(node.nexts.get(c), path, res);
+            path.append(c);
+            traverse(node.children.get(c), path, res);
             // 撤销选择
             path.deleteCharAt(path.length() - 1);
         }
@@ -261,15 +268,15 @@ class TrieMap<V> {
         if (c == '.') {
             // pattern[i] 是通配符，可以变化成任意字符
             // 多叉树（回溯算法）遍历框架
-            for (int j : node.nexts.keySet()) {
-                path.append((char)j);
-                traverse(node.nexts.get(j), path, pattern, i + 1, res);
+            for (char j : node.children.keySet()) {
+                path.append(j);
+                traverse(node.children.get(j), path, pattern, i + 1, res);
                 path.deleteCharAt(path.length() - 1);
             }
         } else {
             // pattern[i] 是普通字符 c
             path.append(c);
-            traverse(node.nexts.get((int)c), path, pattern, i + 1, res);
+            traverse(node.children.get(c), path, pattern, i + 1, res);
             path.deleteCharAt(path.length() - 1);
         }
     }
@@ -297,12 +304,12 @@ class TrieMap<V> {
         char c = pattern.charAt(i);
         // 没有遇到通配符
         if (c != '.') {
-            // 从 node.nexts.get(c) 节点开始匹配 pattern[i+1..]
-            return hasKeyWithPattern(node.nexts.get((int) c), pattern, i + 1);
+            // 从 node.children.get(c) 节点开始匹配 pattern[i+1..]
+            return hasKeyWithPattern(node.children.get(c), pattern, i + 1);
         }
         // 遇到通配符
-        for (int j : node.nexts.keySet()) {
-            if (hasKeyWithPattern(node.nexts.get(j), pattern, i + 1)) {
+        for (int j : node.children.keySet()) {
+            if (hasKeyWithPattern(node.children.get(j), pattern, i + 1)) {
                 return true;
             }
         }
@@ -310,32 +317,32 @@ class TrieMap<V> {
         return false;
     }
 
+    // 返回前缀树中字符串 key 的实例个数
     public int countWordsEqualTo(String key) {
         if (key == null)
             return 0;
         TrieNode<V> node = root;
-        int path = 0;
         for (int i = 0; i < key.length(); i++) {
-            path = key.charAt(i);
-            if (!node.nexts.containsKey(path)) {
+            char path = key.charAt(i);
+            if (!node.children.containsKey(path)) {
                 return 0;
             }
-            node = node.nexts.get(path);
+            node = node.children.get(path);
         }
         return node.end;
     }
 
+    // 返回前缀树中以 prefix 为前缀的字符串个数
     public int countWordsStartingWith(String prefix) {
         if (prefix == null)
             return 0;
         TrieNode<V> node = root;
-        int path = 0;
         for (int i = 0; i < prefix.length(); i++) {
-            path = prefix.charAt(i);
-            if (!node.nexts.containsKey(path)) {
+            char path = prefix.charAt(i);
+            if (!node.children.containsKey(path)) {
                 return 0;
             }
-            node = node.nexts.get(path);
+            node = node.children.get(path);
         }
         return node.pass;
     }
