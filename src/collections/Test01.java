4ace2986701ed44e850c2ab257b447cb671a0113
mrpersimmon <2218443280@qq.com>
2023-04-25 14:22:42 +0800

    #collections#新增#：Person 数组扩容演示代码

---
+++ src/collections/Test01.java
@@ -0,0 +1,19 @@
+package collections;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Person[] pers = new Person[1];
+        pers[0] = new Person();
+
+        Person[] pers2 = new Person[pers.length + 1];
+        for (int i = 0; i < pers.length; i++) {
+            pers2[i] = pers[i];
+        }
+        pers2[pers2.length - 1] = new Person();
+        return;
+    }
+}
+
+class Person {
+
+}
